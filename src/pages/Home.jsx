d7d23ab1d9ad85d1b355c71d77573a98ace6ecf3
ticoniq_ <okolietochi@gmail>
2023-12-19 17:47:28 +0100

    Refactor Navbar and Home components

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -43,7 +43,7 @@ function Home() {
   const [projects, setProjects] = useState([
     {
       id: 1,
-      title: "Bookstore",
+      title: "Bookstore3",
       image: project1,
       details:
         "bookstore React JS project is a web application that allows users to browse and purchase books. The project can be built using React JS, a JavaScript library for building user interfaces.",
@@ -53,7 +53,7 @@ function Home() {
     },
     {
       id: 2,
-      title: "Bookstore",
+      title: "Bookstore2",
       image: project1,
       details:
         "bookstore React JS project is a web application that allows users to browse and purchase books. The project can be built using React JS, a JavaScript library for building user interfaces.",
@@ -63,7 +63,7 @@ function Home() {
     },
     {
       id: 3,
-      title: "Bookstore",
+      title: "Bookstore1",
       image: project1,
       details:
         "bookstore React JS project is a web application that allows users to browse and purchase books. The project can be built using React JS, a JavaScript library for building user interfaces.",
@@ -74,20 +74,6 @@ function Home() {
   ]);
 
   const sortedProjects = [...projects].sort((a, b) => b.id - a.id);
-
-  const fadeInAnimationVarients = {
-    initial: {
-      opacity: 0,
-      y: 100,
-    },
-    animate: (index) => ({
-      opacity: 1,
-      y: 0,
-      transition: {
-        delay: 0.01 * index,
-      },
-    }),
-  };
 
   const techStackToArray = (techStack) => {
     return techStack ? techStack.split(", ") : [];
@@ -152,17 +138,14 @@ function Home() {
         <div className="container py-5 md:py-16">
           <div className="grid gap-10 md:grid-cols-2 lg:grid-cols-3">
             {sortedProjects.map((item, index) => (
-              <div className="" key={item.id}>
+              <div className="" key={item.id} onClick={() => openModal(index)}>
                 <div className="relative mb-6 overflow-hidden bg-cover bg-no-repeat bg-black shadow-lg">
                   <img src={item.image} alt={item.title} className="w-full" />
-                  {/* <a onClick={() => openModal(index)}>
-                    <div className="mask absolute inset-0 h-full w-full overflow-hidden bg-fixed opacity-0 transition duration-300 ease-in-out hover:opacity-100 bg-[hsla(0,0%,98.4%,0.2)]" />
-                  </a> */}
                     <div
                       className="absolute inset-0 h-full w-full overflow-hidden bg-fixed transition duration-300 ease-in-out bg-[hsla(0,0%,99%,0.15)]">
                         <div className="flex h-full items-start justify-start">
                           <div className="m-6 text-white">
-                            <h5 className="mb-3 text-lg font-bold">{item.title}</h5>
+                            <h5 className="mb-3 text-lg font-bold cursor-pointer" onClick={() => openModal(index)}>{item.title}</h5>
                             {techStackToArray(item.techStack).map((tech, index) => (
                               <span
                                 key={index}
@@ -195,6 +178,78 @@ function Home() {
               </div>
             ))}
           </div>
+          {/* Modal Popup */}
+          {selectedProjectIndex !== null && (
+            <div className="fixed inset-0 grid place-items-center z-50 max-h-full bg-black bg-opacity-75 overflow-y-scroll">
+              <div
+                className="max-w-5xl bg-white dark:bg-neutral rounded-lg m-5">
+                <article className="grid grid-cols-1 gap-5 md:grid-cols-2">
+                  <figure className="bg-black">
+                    <img
+                      src={selectedProject.image}
+                      alt={selectedProject.title}
+                      className="rounded-lg shadow-lg w-full h-full"
+                    />
+                  </figure>
+                  <aside className="p-4">
+                    <div className="flex justify-end items-center">
+                      <XMarkIcon
+                        className="w-6 cursor-pointer"
+                        onClick={closeModal}
+                      />
+                    </div>
+                    <h2 className="text-2xl font-bold">
+                      {selectedProject.title}
+                    </h2>
+                    <p className="mt-4">
+                      {selectedProject.details}
+                    </p>
+                    <div className="my-5">
+                      {techStackToArray(selectedProject.techStack).map(
+                        (tech, index) => (
+                          <span
+                            key={index}
+                            className="mr-2 my-1 border-2 badge badge-outline font-semibold">
+                            {tech}
+                          </span>
+                        )
+                      )}
+                    </div>
+                    <div className="mt-4 flex justify-start gap-5 text-sm">
+                      <a
+                        href={selectedProject.source}
+                        rel="noreferrer"
+                        target="_blank"
+                        className="font-bold flex gap-1 hover:text-newYellow">
+                        <CodeBracketIcon className="w-4" />
+                        Source Code
+                      </a>
+                      <a
+                        href={selectedProject.live}
+                        rel="noreferrer"
+                        target="_blank"
+                        className="text-newDarkGray font-bold flex gap-1 dark:text-white hover:text-newYellow dark:hover:text-newYellow">
+                        <EyeIcon className="w-4" />
+                        Live Preview
+                      </a>
+                    </div>
+                    <div className="flex justify-between mt-6 relative top-0">
+                      <button
+                        onClick={prevProject}
+                        className="text-newDarkGray font-semibold dark:text-white hover:text-newYellow dark:hover:text-newYellow">
+                        Previous
+                      </button>
+                      <button
+                        onClick={nextProject}
+                        className="text-newDarkGray font-semibold dark:text-white hover:text-newYellow dark:hover:text-newYellow">
+                        Next
+                      </button>
+                    </div>
+                  </aside>
+                </article>
+              </div>
+            </div>
+          )}
           <div className="flex flex-col justify-center items-center gap-10 mt-10 md:flex-row">
             <Link
               to="/project"
